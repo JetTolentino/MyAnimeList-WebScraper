77f15b7f5126d973b1f6c96c802accd57f7e68a5
Jet Tolentino <tolentinogetulius@gmail.com>
2020-09-29 21:07:23 +0800

    added user input

--- scraper.py
+++ scraper.py
@@ -1,16 +1,7 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
-import requests
-
-my_url = 'https://myanimelist.net/anime/genre/6/Demons?page=1'
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
-
-page_soup = soup(page_html , 'html.parser')  #HTML parser 
-containers = page_soup.findAll('div', {'class': 'seasonal-anime js-seasonal-anime'})     #anime list
-
-container = containers[0]  
+import sys
+  
 def get_title(container):
     title = container.div.div.h2.a.get_text()
     return title
@@ -20,11 +11,16 @@ def get_studio(container):
     return studio
 
 def get_episodes(container):
-    episodes = container.find('div',{'class': 'eps'}).get_text().strip()   
-    return episodes
-
-genre_container = container.find('div', {'class': 'genres-inner js-genre-inner'}) #finds the genre container
-genres = genre_container.findAll('a') #this is a list of genres 
+    episodes = container.find('div',{'class': 'eps'}).get_text().strip()
+    try:
+        episode =int(episodes.replace(' eps', ''))
+    except:
+        try:
+            episode =int(episodes.replace(' ep', ''))
+        except:
+            episode = "Unavailable"
+    
+    return episode
 
 def get_genre(container):
     genre_container = container.find('div', {'class': 'genres-inner js-genre-inner'}) #finds the genre container
@@ -37,22 +33,129 @@ def get_genre(container):
         ptr+=1
     return genre
 
-def get_data():
-    my_url = 'https://myanimelist.net/anime/genre/6/Demons?page=1'
-    uClient = uReq(my_url)
-    page_html = uClient.read()
-    uClient.close()
+def get_score(container):
+    score = container.find('span', {'title': 'Score'}).get_text().strip()
+    return score
 
-    page_soup = soup(page_html , 'html.parser')  #HTML parser 
-    containers = page_soup.findAll('div', {'class': 'seasonal-anime js-seasonal-anime'})     #anime list
+def get_data(genre):
+    page = 1
+    end_of_pages = False
+    num_anime = 0
+    genres_key = {"action":"1",
+              "adventure": "2",
+              "cars": "3",
+              "comedy":"4",
+              "dementia":"5",
+              "demons":"6",
+              "drama": "8",
+              "ecchi": "9",
+              "fantasy": "10",
+              "game":"11",
+              "harem":"35",
+              "historical":"13",
+              "horror":"14",
+              "josei": "43",
+              "kids": "15",
+              "magic": "16",
+              "martial arts": "17",
+              "mecha": "18",
+              "military": "38",
+              "music":"19",
+              "mystery":"7",
+              "parody":"20",
+              "police":"39",
+              "psychological": "40",
+              "romance": "22",
+              "samurai":"21",
+              "school":"23",
+              "sci-fi":"24",
+              "seinen": "42",
+              "shoujo":"25",
+              "shoujo-ai":"26",
+              "shounen": "27",
+              "shounen-ai":"28",
+              "slice of life":"36",
+              "space": "29",
+              "sports": "30",
+              "super power": "31",
+              "supernatural":"37",
+              "thriller":"41",
+              "vampire":"32"}
 
-    #container = containers[0]   
-    for container in containers:
-        print(get_title(container))
-        print(get_studio(container))
-        print(get_episodes(container))
-        print(get_genre(container))
+    genres = {"action":"action",
+              "adventure": "adventure",
+              "cars": "cars",
+              "comedy":"comedy",
+              "dementia":"dementia",
+              "demons":"demons",
+              "drama": "drama",
+              "ecchi": "ecchi",
+              "fantasy": "fantasy",
+              "game":"game",
+              "harem":"harem",
+              "historical":"historical",
+              "horror":"horror",
+              "josei": "josei",
+              "kids": "kids",
+              "magic": "magic",
+              "martial arts": "martial_arts",
+              "mecha": "mecha",
+              "military": "military",
+              "music":"music",
+              "mystery":"mystery",
+              "parody":"parody",
+              "police":"police",
+              "psychological": "psychological",
+              "romance": "romance",
+              "samurai":"samurai",
+              "school":"school",
+              "sci-fi":"sci_fi",
+              "seinen": "seinen",
+              "shoujo":"shoujo",
+              "shoujo-ai":"shoujo_ai",
+              "shounen": "shounen",
+              "shounen-ai":"shounen_ai",
+              "slice of life":"slice_of_life",
+              "space": "space",
+              "sports": "sports",
+              "super power": "super_power",
+              "supernatural":"supernatural",
+              "thriller":"thriller",
+              "vampire":"vampire"}
+    while not end_of_pages:
+        page_string= str(page)
+        try:
+            my_url = f'https://myanimelist.net/anime/genre/{genres_key[genre]}/{genres[genre]}?page={page_string}'
+            uClient = uReq(my_url)
+            page_html = uClient.read()
+            uClient.close()
+            page_soup = soup(page_html , 'html.parser')  #HTML parser 
+            containers = page_soup.findAll('div', {'class': 'seasonal-anime js-seasonal-anime'})
+            page += 1
+
+        except:
+            end_of_pages = True
+
+        #container = containers[0]   
+        for container in containers:
+            print(get_title(container))
+            print(get_studio(container))
+            print(get_episodes(container))
+            print(get_genre(container))
+            print(get_score(container))
+        if not end_of_pages:    
+            num_anime += len(containers) #number of anime in a page
+        print(my_url)
+        print(num_anime)
+        print(f"{genres[genre]}")
+
+
     
-    print(len(containers))
 
-get_data()
+
+
+
+
+if __name__ == "__main__":
+    genre = input("Enter genre: ")
+    get_data(genre)
